31c284e8746a93c67d7a8c750193f170acb5750f
RAKESH M B <118419906+RakeshCherry@users.noreply.github.com>
2023-12-19 15:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const nonRepeatedChar = new Map();
+	for(const char of str){
+		nonRepeatedChar.set(char, (nonRepeatedChar.get(char) || 0) + 1);
+	}
+
+	for (const char of str){
+		if(nonRepeatedChar.get(char) === 1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input)) 
